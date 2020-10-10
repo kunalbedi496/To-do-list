14e6d5aefa21a41ce3cdd09f3e277d36c260acf2
kunalbedi496 <kunalbedi496@gmail.com>
2020-10-10 16:21:41 +0530

    Updates

---
+++ precache-manifest.1dfbfb3a0e878aeba1f0de08d77fa2bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5deb7593f5ae79195886d5165a90b47d",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "5c2e69b82f49b6bb3dd3",
+    "url": "/To-do-list/static/css/main.66d69090.chunk.css"
+  },
+  {
+    "revision": "60b89832e5c848c3337c",
+    "url": "/To-do-list/static/js/2.f24ec908.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list/static/js/2.f24ec908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2e69b82f49b6bb3dd3",
+    "url": "/To-do-list/static/js/main.5f7b2a43.chunk.js"
+  },
+  {
+    "revision": "b594183603d53eb80d72",
+    "url": "/To-do-list/static/js/runtime-main.24227ee6.js"
+  }
+]);
